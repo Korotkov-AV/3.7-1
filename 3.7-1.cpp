66b98d38223252109e4d37b7f5e80a0a41402c74
Alex <korotkov.aw@gmail.com>
2023-07-27 06:21:57 +0500

    Initial commit

---
+++ 3.7-1.cpp
@@ -0,0 +1,38 @@
+﻿// 3.7-1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#define MODE 5
+
+#ifndef MODE
+#error needs mode
+#endif 
+void add(int a, int b) {
+    std::cout << a + b;
+}
+
+
+
+int main(){
+#if MODE==0
+std::cout << "train mode ";
+
+
+#elif MODE==1
+    std::cout << "W mode ";
+    int a, b;
+    std::cout << "input 2 numbers \n";
+    std::cin >> a >> b;
+    add(a, b);
+# else 
+    std::cout << "unknown MODE ";
+
+#endif
+
+
+
+
+
+ 
+}
+
